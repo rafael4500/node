568f119a28c88f5b83df38e85d57acca525dbc1b
Robson Feitosa <robson.feitosa@tegra.com.br>
2023-03-23 11:32:13 -0300

    Novas atividades 2

---
+++ src/AtividadesPraFazer.js
@@ -0,0 +1,37 @@
+/**Ralize as seguintes atividades */
+
+const carros = [
+    {
+        id: 1,
+        modelo: 'Maverick',
+        marca: 'Chevrolet',
+        ano: 2002
+    },
+    {
+        id: 2,
+        modelo: 'Fusca',
+        marca: 'fiat',
+        ano: 2015
+    }
+]
+
+
+const carroGol = {
+    id: 3,
+    modelo: 'gol',
+    marca: 'Volkswagen',
+    ano: 2020
+} 
+/**
+ *   
+ *  1 - Mude a marca de 'fiat' para 'Volkswagen' da matriz de carrosAntigos. E lance para o repositorio
+ * 
+ * 
+ *  2 - Para o objeto do id 1 mude o ano do carro para 2005
+ * 
+ * 
+ *  3 - Adicione o novo objeto "carroGol:  para a matriz "carros"
+ * 
+ * 
+ * 
+ */
